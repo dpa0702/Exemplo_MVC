ff24619d25db4055f4ee859da06269a027878abd
admin <admin@DESKTOP-D57T29K>
2022-11-30 14:55:57 -0200

    Adicionar arquivos de projeto.

---
+++ Exemplo_MVC/Models/Conta.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Exemplo_MVC.Models
+{
+    [Table("Conta")]
+    public class Conta
+    {
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Column("Descricao")]
+        [Display(Name = "Descricao")]
+        public string Description { get; set; }
+
+    }
+}
---
+++ Exemplo_MVC/Models/Contexto.cs
@@ -0,0 +1,9 @@
+﻿namespace Exemplo_MVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Conta> Contas { get; set; }
+    }
+}
